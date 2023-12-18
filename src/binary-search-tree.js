377b0331f360d399ef0727a2e3fc8bdd3f49b5bf
Sergii Kolesnyk <marakorissk@gmail.com>
2023-12-19 00:59:17 +0200

    docs: tree fifnished

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -8,12 +8,12 @@ const { Node } = require('../extensions/list-tree.js');
 */
 
 class BinarySearchTree {
-  constructor() {}
+  constructor() {
+    this.treeRoot = null
+  }
 
   root() {
-    this.data = null;
-    this.left = null;
-    this.right = null;
+    return this.treeRoot;
   }
 
   add(data) {
@@ -23,13 +23,12 @@ class BinarySearchTree {
       return;
     }
 
-    if (!this.root) {
-      this.root = newNode;
-      this.root() = newNode;
+    if (!this.treeRoot) {
+      this.treeRoot = newNode;
       return;
     }
 
-    let currentNode = this.root;
+    let currentNode = this.treeRoot;
 
     while(currentNode){
       if (newNode.data < currentNode.data) {
@@ -49,7 +48,15 @@ class BinarySearchTree {
   } 
 
   has(data) {
-    let currentData = this.root;
+    let currentData = this.treeRoot;
+
+    if (currentData == null) {
+      return false
+    }
+
+    if (currentData == data) {
+      return true
+    }
 
     if (currentData.data == data) {
       return true;
@@ -76,44 +83,166 @@ class BinarySearchTree {
     }
     return false;
   }
+  
+  find(data) {
+    let currentData = this.treeRoot;
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    if (currentData == null) {
+      return null;
+    }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (currentData.data == data) {
+      return currentData;
+    }
+
+    while (currentData) {
+      if(currentData.data > data) {
+        if (!currentData.left) {
+          return null;
+        }
+        if (currentData.left.data == data) {
+          return currentData.left;
+        }
+        currentData = currentData.left;
+      } else {
+        if (!currentData.right) {
+          return null;
+        }
+        if (currentData.right.data == data) {
+          return currentData.right;
+        }
+        currentData = currentData.right;
+      }
+    }
+    return null;
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let currentData = this.treeRoot;
+    while (currentData != null) {
+      if (currentData.left == null) {
+        return currentData.data
+      } else {
+        currentData = currentData.left
+      }
+    }
+    return null;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let currentData = this.treeRoot;
+    while (currentData != null) {
+      if (currentData.right == null) {
+        return currentData.data
+      } else {
+        currentData = currentData.right
+      }
+    }
+    return null;
   }
+
+  remove(data) {
+
+    if (this.treeRoot == null) {
+      return;
+    }
+
+    function nodeHist (data, root) {
+      let currentData = root;
+  
+      if (currentData.data == data) {
+        return [currentData, null];
+      }
+  
+      while (currentData) {
+        if(currentData.data > data) {
+          if (!currentData.left) {
+            return null;
+          }
+          if (currentData.left.data == data) {
+            return [currentData.left, currentData];
+          }
+          currentData = currentData.left;
+        } else {
+          if (!currentData.right) {
+            return null;
+          }
+          if (currentData.right.data == data) {
+            return [currentData.right, currentData];
+          }
+          currentData = currentData.right;
+        }
+      }
+      return null;
+    };
+
+    // if (data == this.treeRoot.data) {
+    //   console.log('del: root');
+    //   this.treeRoot = null;
+    //   return;
+    // }
+
+    let [findNode, parentNode] = nodeHist(data, this.treeRoot);
+
+    if(findNode != null) {
+      if (findNode.left == null && findNode.right == null) {
+        // case 1
+        if (parentNode.left == findNode) {
+          findNode = null;
+          parentNode.left = null;
+        } else {
+          findNode = null;
+          parentNode.right = null;
+        }
+        return;
+      }
+      if(findNode.left != null && findNode.right != null) {
+        //case 3
+        let leftTreeMax;
+        let startNode = findNode.left;
+        while (startNode) {
+          if (startNode.right != null) {
+            startNode = startNode.right;
+          } else {
+            leftTreeMax = startNode.data;
+            startNode = null;
+          }
+        }
+        this.remove(leftTreeMax);
+        findNode.data = leftTreeMax;
+        return;
+      } else {
+        // case 2 -- left
+        if (findNode.left != null) {
+          if (parentNode.left == findNode) {
+            parentNode.left = findNode.left;
+            findNode = null;
+          } else {
+            parentNode.right = findNode.left;
+            findNode = null;
+          }
+          return;
+          // case 2 -- right
+        } else if (findNode.right != null) {
+          if (parentNode.left == findNode) {
+            parentNode.left = findNode.right;
+            findNode = null;
+          } else {
+            parentNode.right = findNode.right;
+            findNode = null;
+          }
+          return;
+        }
+      }
+    } else {
+      console.log('cant find item');
+      return;
+    }
+  }
+
 }
 
-const myTree = new BinarySearchTree();
-myTree.add(8);
-myTree.add(1);
-myTree.add(3);
-myTree.add(11);
-myTree.add(5);
-myTree.add(9);
-myTree.add(15);
-myTree.add(13);
-
-console.log(myTree.root().data);
-
-console.log(myTree.has(11));
-
-// console.log(myTree);
-
+// const myTree = new BinarySearchTree();
 
 
 module.exports = {
