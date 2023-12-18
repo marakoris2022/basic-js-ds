c014e1e4235d8dd2cb53a5783b545ab03b58fa2f
Sergii Kolesnyk <marakorissk@gmail.com>
2023-12-18 14:29:51 +0200

    docs: reset

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,26 +1,80 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
 class BinarySearchTree {
+  constructor() {}
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.data = null;
+    this.left = null;
+    this.right = null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  add(data) {
+    const newNode = new Node(data);
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!data && data < 0) {
+      return;
+    }
+
+    if (!this.root) {
+      this.root = newNode;
+      this.root() = newNode;
+      return;
+    }
+
+    let currentNode = this.root;
+
+    while(currentNode){
+      if (newNode.data < currentNode.data) {
+        if (!currentNode.left) {
+          currentNode.left = newNode;
+          return;
+        }
+        currentNode = currentNode.left;
+      } else {
+        if (!currentNode.right) {
+          currentNode.right = newNode;
+          return;
+        }
+        currentNode = currentNode.right;
+      }
+    }
+  } 
+
+  has(data) {
+    let currentData = this.root;
+
+    if (currentData.data == data) {
+      return true;
+    }
+
+    while (currentData) {
+      if(currentData.data > data) {
+        if (!currentData.left) {
+          return false;
+        }
+        if (currentData.left.data == data) {
+          return true;
+        }
+        currentData = currentData.left;
+      } else {
+        if (!currentData.right) {
+          return false;
+        }
+        if (currentData.right.data == data) {
+          return true;
+        }
+        currentData = currentData.right;
+      }
+    }
+    return false;
   }
 
   find(/* data */) {
@@ -43,6 +97,24 @@ class BinarySearchTree {
     // remove line with error and write your code here
   }
 }
+
+const myTree = new BinarySearchTree();
+myTree.add(8);
+myTree.add(1);
+myTree.add(3);
+myTree.add(11);
+myTree.add(5);
+myTree.add(9);
+myTree.add(15);
+myTree.add(13);
+
+console.log(myTree.root().data);
+
+console.log(myTree.has(11));
+
+// console.log(myTree);
+
+
 
 module.exports = {
   BinarySearchTree
