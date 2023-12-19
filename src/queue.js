8ae93b6a52d8f3b7de2f7049f1d283978e25883f
Sergii Kolesnyk <marakorissk@gmail.com>
2023-12-19 15:23:27 +0200

    docs: Queue ready

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,22 +14,96 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.elements = 0;
+    this.head = null;
+    this.tail = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    // Обратный список
+    function reverseList(item) {
+      let currNode = item.tail;
+      let prevNode = null;
+      let nextNode = null;
+
+      while (currNode) {
+        nextNode = currNode.next;
+        currNode.next = prevNode;
+        prevNode = currNode;
+        currNode = nextNode;
+      }
+      item.tail = item.head;
+      item.head = prevNode;
+      return item.tail;
+    }
+    return reverseList(this);
+  }
+
+  enqueue(value) {
+    const newNode = new ListNode(value);
+    if (this.elements == 0) {
+      this.head = newNode;
+      this.tail = newNode;
+      newNode.next = null;
+      this.elements += 1;
+      return;
+    }
+    if (this.elements == 1) {
+      this.tail = newNode;
+      newNode.next = this.head;
+      this.elements += 1;
+      return;
+    }
+    if (this.elements > 1) {
+      const oldTail = this.tail;
+      this.tail = newNode;
+      newNode.next = oldTail;
+      this.elements += 1;
+      return;
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.elements == 0) {
+      return;
+    }
+    if (this.elements == 1) {
+      const tempValue = this.head.value;
+      this.head = null;
+      this.tail = null;
+      this.elements -= 1;
+      return tempValue;
+    }
+    if (this.elements > 1) {
+      let headParent;
+      let currentNode = this.tail;
+      while (currentNode != this.head) {
+        if (currentNode.next == this.head) {
+          headParent = currentNode;
+          break;
+        } else {
+          currentNode = currentNode.next;
+        }
+      }
+      const tempValue = this.head.value;
+      this.head = headParent;
+      this.head.next = null;
+      this.elements -= 1;
+      return tempValue;
+    }
+
   }
 }
+
+// const myQueue = new Queue();
+// myQueue.enqueue(1);
+// myQueue.enqueue(2);
+// myQueue.enqueue(3);
+// myQueue.enqueue(1);
+// myQueue.enqueue(2);
+// console.log(myQueue);
+// myQueue.getUnderlyingList();
 
 module.exports = {
   Queue
